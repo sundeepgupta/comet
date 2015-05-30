ddb139c7695b08e0e3b522e8b67b830dee478190
Sundeep Gupta <sundeep@sundeepgupta.ca>
2015-05-29 20:47:21 -0400

    Started project.

---
+++ CometTests/CometTests.swift
@@ -0,0 +1,36 @@
+//
+//  CometTests.swift
+//  CometTests
+//
+//  Created by Sundeep Gupta on 2015-05-29.
+//  Copyright (c) 2015 Sundeep Gupta. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CometTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
